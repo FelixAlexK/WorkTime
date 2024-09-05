c1941a596a8e28a7b4de85de111431593179a815
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2024-09-05 18:31:30 +0200

    feat: add type prop

--- src/components/ButtonComponent.vue
+++ src/components/ButtonComponent.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
-defineProps<{ outlined: boolean, label: string }>()
+const { outlined = false, label, type = 'button' } = defineProps<{ outlined?: boolean, label?: string, type?: "submit" | "reset" | "button" }>()
 defineEmits(['action'])
 </script>
 
 <template>
-    <button @click="$emit('action')"
+    <button @click="$emit('action')" :type="type"
         :class="{ 'border-gray-500 border-2 hover:bg-gray-500 hover:text-white text-gray-500 ': outlined, 'bg-black text-white hover:bg-gray-700': !outlined }"
-        class=" rounded px-4 py-1 flex flex-row gap-2 items-center">
+        class=" rounded px-4 py-1 flex flex-row gap-2 items-center justify-center">
         <div v-if="$slots.icon" class="">
 
             <slot name="icon"></slot>
         </div>
-        <p class="hidden sm:block">
+        <p v-if="label" class="hidden md:block">
             {{ label }}
         </p>
     </button>
