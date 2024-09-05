7e18e3f77d3a7f1f911d05ae314acc2cf7f2bf82
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2024-09-05 19:00:04 +0200

    6 add start and end time manually (#19)
    * 14 fix UI (#16)
    
    * feat: add methods
    
    * feat: add id prop
    
    * feat: add query
    
    * style: add new style
    
    * feat: add button component
    
    * feat: add page header component
    
    * feat: add project item component
    
    * feat: add time entry component
    
    * feat: add live time tracker (#18)
    
    * chore: update dependencies
    
    * chore: update dependencies
    
    * feat: add drawer
    
    * feat: mutation
    
    * feat: add type prop
    
    * feat: add action event
    
    * chore: set outlined
    
    * feat: add auto-animate
    
    * style: change style of outlined button
    
    * feat: add animation

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const { outlined = false, label, type = 'button' } = defineProps<{ outlined?: boolean, label?: string, type?: "submit" | "reset" | "button" }>()
+defineEmits(['action'])
+</script>
+
+<template>
+    <button @click="$emit('action')" :type="type"
+        :class="{ 'border-black border-2 hover:bg-black hover:text-white text-black ': outlined, 'border-black border-2 hover:bg-white hover:text-black text-white bg-black': !outlined }"
+        class=" rounded px-4 py-1 flex flex-row gap-2 items-center justify-center">
+        <div v-if="$slots.icon" class="">
+
+            <slot name="icon"></slot>
+        </div>
+        <p v-if="label" class="hidden md:block">
+            {{ label }}
+        </p>
+    </button>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 import { createConvexVue } from '@convex-vue/core'
 
 const convexVue = createConvexVue({
@@ -12,7 +12,7 @@ const convexVue = createConvexVue({
 
 const app = createApp(App)
 app.use(convexVue)
-
+app.use(autoAnimatePlugin)
 app.use(router)
 
 app.mount('#app')
